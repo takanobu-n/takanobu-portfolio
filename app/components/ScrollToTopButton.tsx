83b0b5731813eac823c916a217fb20a50d5dfaac
takanobu <takanobu.n.22@gmail.com>
2024-12-14 00:11:08 +0900

    Readmeを追記

--- app/components/ScrollToTopButton.tsx
+++ app/components/ScrollToTopButton.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { FaArrowUp } from 'react-icons/fa';
 
 const ScrollToTopButton: React.FC = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -23,14 +24,14 @@ const ScrollToTopButton: React.FC = () => {
 
   return (
     <div>
-      {isVisible && (
-        <button
-          onClick={scrollToTop}
-          className="fixed bottom-4 right-4 p-3 bg-blue-600 text-white rounded-full shadow-md hover:bg-blue-700 focus:outline-none"
-        >
-          ↑
-        </button>
-      )}
+      <button
+        onClick={scrollToTop}
+        className={`fixed bottom-4 right-4 w-12 h-12 bg-white text-gray-800 border-2 border-gray-800 rounded-full flex items-center justify-center shadow-md hover:bg-blue-100 focus:outline-none transition-all duration-500 transform ${
+          isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0 pointer-events-none'
+        }`}
+      >
+        <FaArrowUp className="text-lg" />
+      </button>
     </div>
   );
 };
